a1bea77406dfe8fe83769a11db981328c9df3661
PinQiH <cassie34131@gmail.com>
2023-09-14 16:27:33 +0800

    update

--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,8 @@
         <p>&copy; <?php echo date("Y"); ?> ScentSelect。保留所有權利。</p>
         <p>聯繫我們：contact@example.com</p>
         <ul class="footer-links">
-            <li><a href="../privacy.php">隱私政策</a></li>
-            <li><a href="../terms.php">使用條款</a></li>
+            <li><a href="./privacy.php">隱私政策</a></li>
+            <li><a href="./terms.php">使用條款</a></li>
         </ul>
     </div>
 </footer>
